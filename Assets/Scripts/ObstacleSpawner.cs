051b2c93ec8a09e79e1f49ff017b32f8b62609a4
UnexpectedVariable <rodionp49@gmail.com>
2024-07-11 20:58:07 +0300

    Obstacle spawn logic
    -Added coin prefab
    -Setup logic for spawning walls

---
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [RequireComponent(typeof(Collider))]
+    internal class ObstacleSpawner : MonoBehaviour
+    {
+        [SerializeField]
+        private Wall _wall = null;
+        [SerializeField]
+        private GameObject _pool = null;
+
+        /*[SerializeField]
+        private int _minimalGapsPerWave = 0;*/
+        [SerializeField]
+        private int _maximumGapsPerWave = 0;
+
+        private System.Random _rng = null;
+        private int _positionCount = 0;
+
+        public System.Random RNG
+        {
+            set => _rng = value;
+        }
+        public int PositionCount
+        {
+            set => _positionCount = value;
+        }
+
+        private void Start()
+        {
+            /*if(_maximumGapsPerWave < _minimalGapsPerWave)
+            {
+                throw new ArgumentException("Maximum range value for gaps per wave can't be lower than minimal value");
+            }*/
+
+            if (_maximumGapsPerWave <= 0) Debug.LogWarning($"Maximum gap count per wave is lower or equals zero");
+
+            _rng = new System.Random();
+        }
+
+        [ContextMenu("Spawn")]
+        public void Spawn()
+        {
+            if (_rng == null) return;
+
+            var gapsPositions = GenerateGapsPositions();
+
+            Wall wall = Instantiate(_wall, transform.position, transform.rotation, _pool.transform);
+
+        }
+
+        private int[] GenerateGapsPositions()
+        {
+            int[] positions = new int[_maximumGapsPerWave];
+
+            for(int i = 0; i < positions.Length; i++)
+            {
+                positions[i] = _rng.Next(_positionCount + 1);
+            }
+
+            return positions;
+        }
+    }
+}
---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    internal class Wall : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject[] _pieces = null;
+
+        public void Disable(int[] indexes)
+        {
+            foreach(var index  in indexes)
+            {
+                _pieces[index].SetActive(false);
+            }
+        }
+
+        public void Enable()
+        {
+            foreach(var piece in _pieces)
+            {
+                piece.SetActive(true);
+            }
+        }
+    }
+}
